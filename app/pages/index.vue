59dab27334d84dbd55d8f9d8b463392864029cf1
Andre Thomas <drethedevjs@gmail.com>
2025-10-22 14:20:04 -0400

    Initial commit.

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>
+	<h1>Hi mom</h1>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
+	modules: ["@nuxt/eslint", "@nuxt/ui", "nuxt-og-image", "@nuxtjs/sitemap", "nuxt-seo-utils"],
+	css: ["~/assets/css/main.css"],
+	vite: {
+		plugins: [tailwindcss()]
+	},
+	site: {
+		// og Image config
+		url: "https://example.com",
+		name: "Made Worthy Media"
+	},
+	app: {
+		head: {
+			title: "Made Worthy Media", // default fallback title
+			htmlAttrs: {
+				lang: "en"
+			}
+		}
+	}
+});
