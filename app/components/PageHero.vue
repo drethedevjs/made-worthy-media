c460f77ff7875819b95f56206d19bb88d1958133
Andre Thomas <drethedevjs@gmail.com>
2025-11-20 17:53:38 -0500

    Swapped the NuxtUI page hero for a custom one.

--- app/components/PageHero.vue
+++ app/components/PageHero.vue
@@ -1,10 +1,26 @@
 <template>
-	<UPageHero
-		title="Made Worthy Media"
-		description="Wedding content creation + Super 8 film"
-		headline="Candidly Captured. Eternally Worthy."
-		class="w-full lg:h-screen bg-center bg-[url(https://www.theknot.com/tk-media/images/51c0576d-0a74-42b7-9137-7b29b162ead0)] bg-cover"
-	></UPageHero>
+	<div
+		class="relative w-full h-screen bg-cover bg-center"
+		style="
+			background-image: url(https://www.theknot.com/tk-media/images/51c0576d-0a74-42b7-9137-7b29b162ead0);
+		"
+	>
+		<!-- Overlay -->
+		<div class="absolute inset-0 bg-black opacity-40"></div>
+
+		<!-- Centered Content -->
+		<div class="absolute inset-0 flex flex-col items-center justify-center">
+			<h1 class="text-6xl lg:text-8xl font-bold text-white text-center text-shadow-2xs">
+				Made Worthy Media
+			</h1>
+			<h2 class="text-lg lg:text-3xl text-white text-center mt-4 text-shadow-2xs">
+				Wedding content creation + Super 8 film
+			</h2>
+			<h2 class="text-xl lg:text-4xl text-brand-light text-center mt-8 font-light text-shadow-2xs">
+				Candidly Captured. Eternally Worthy.
+			</h2>
+		</div>
+	</div>
 </template>
 
 <style lang="css" scoped></style>
