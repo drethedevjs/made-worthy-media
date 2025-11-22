23de385491ad488ea1b1b8612fea469a06675e3c
Andre Thomas <drethedevjs@gmail.com>
2025-11-22 16:26:22 -0500

    Added links to videos.

--- app/pages/super8.vue
+++ app/pages/super8.vue
@@ -1,35 +1,58 @@
 <script lang="ts"></script>
 
 <template>
-	<section id="super-8" class="bg-accent h-screen">
-		<div class="w-full flex flex-col lg:flex-row h-full">
-			<div class="w-full lg:w-1/2 flex flex-col space-y-6 my-auto">
-				<div class="w-full">
-					<div class="bg-brand-sage rounded-md h-96 w-96 place-self-center">video goes here</div>
-				</div>
-				<div class="w-full">
-					<div class="bg-brand-sage rounded-md h-96 w-96 place-self-center">video goes here</div>
+	<section
+		id="super-8"
+		class="bg-accent lg:h-screen w-full flex flex-col-reverse lg:flex-row pb-10"
+	>
+		<div class="w-full lg:w-1/2 flex flex-col space-y-6 my-auto">
+			<div class="w-full">
+				<div class="flex justify-center mt-6">
+					<div class="rounded-md overflow-hidden w-96 h-96">
+						<iframe
+							src="https://drive.google.com/drive/folders/1923SbfImF62Y2qHfDCxHxnOQZBg9AOKw"
+							class="w-full h-full"
+							frameborder="0"
+							allow="autoplay; encrypted-media; fullscreen"
+							allowfullscreen
+							title="Super 8 sample video"
+						></iframe>
+					</div>
 				</div>
 			</div>
+			<div class="w-full">
+				<div class="flex justify-center mt-6">
+					<div class="rounded-md overflow-hidden w-96 h-96">
+						<iframe
+							src="https://drive.google.com/drive/folders/1923SbfImF62Y2qHfDCxHxnOQZBg9AOKw"
+							class="w-full h-full"
+							frameborder="0"
+							allow="autoplay; encrypted-media; fullscreen"
+							allowfullscreen
+							title="Super 8 sample video"
+						></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
 
-			<div class="w-full lg:w-1/2 p-10 space-y-5 text-brand-light">
-				<h1 class="text-brand-light page-header mb-10">Super 8</h1>
-				<p>
-					Super 8 is my absolute favorite way to document someone's wedding day.. I oftentimes say
-					that The magic of Super 8 lies in how it makes you feel. Unlike traditional wedding
-					videos, it doesn't just replay your day, but draws you back into it. It's less about
-					watching a film and more about stepping into the memory itself.
-				</p>
-				<p>
-					Super 8 is a style of video making that uses 8mm Kodak film. these are perfect for the
-					couples who want a wedding video that carries the intimacy of a home video, wrapped in the
-					texture and grace of a cinematic film.
-				</p>
-				<p>
-					All packages can be customized to your day and what your needs are. But this will give us
-					a great starting point so that we can build a package that meets those needs.
-				</p>
-			</div>
+		<div class="w-full lg:w-1/2 p-10 space-y-5 md:place-content-center text-brand-light">
+			<h1 class="text-brand-light page-header mb-10 text-6xl! lg:text-9xl!">Super 8</h1>
+			<p>
+				Super 8 is my absolute favorite way to document someone's wedding day.. I oftentimes say
+				that The magic of Super 8 lies in how it makes you feel. Unlike traditional wedding videos,
+				it doesn't just replay your day, but draws you back into it. It's less about watching a film
+				and more about stepping into the memory itself.
+			</p>
+			<p>
+				Super 8 is a style of video making that uses 8mm Kodak film. these are perfect for the
+				couples who want a wedding video that carries the intimacy of a home video, wrapped in the
+				texture and grace of a cinematic film.
+			</p>
+			<p>
+				All packages can be customized to your day and what your needs are. But this will give us a
+				great starting point so that we can build a package that meets those needs.
+			</p>
 		</div>
 	</section>
 </template>
